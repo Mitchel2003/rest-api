198c6d3ea7471349715763072a1aa68c7f22f800
mitchel2003 <avilesmaicol.08@gmail.com>
2024-09-15 17:07:17 -0500

    update => port assignment

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,4 @@ import app from "./app";
 import "dotenv/config"
 
 connectionDB();
-app.listen(process.env.PORT);
-console.log(`Server initialized on port ${process.env.PORT}`);
+app.listen(process.env.PORT, () => console.log(`Server initialized`));

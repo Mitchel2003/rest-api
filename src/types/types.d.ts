cd849bc95833d7575182042fd9b90d69e15d366a
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-22 14:11:34 -0500

    update => custom tsconfig.json and set types.d.ts

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,3 +3,12 @@ declare module 'express';
 declare module 'cors';
 declare module 'jsonwebtoken';
 declare module 'bcryptjs';
+
+// Extendemos la interfaz Request de Express para incluir las propiedades personalizadas
+declare namespace Express {
+  export interface Request {
+    user?: { id: string };
+    userReferences?: import('../interfaces/props.interface').UserReferencesProps;
+    token?: any;
+  }
+}

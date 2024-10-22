8723db333ff24c594c298a24df8b5caf574d5108
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-22 17:21:05 -0500

    update => test

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,6 @@
-declare module 'cookie-parser';
-declare module 'express';
-declare module 'cors';
-declare module 'jsonwebtoken';
-declare module 'bcryptjs';
-declare module "dotenv/config";
+// declare module 'cookie-parser';
+// declare module 'express';
+// declare module 'cors';
+// declare module 'jsonwebtoken';
+// declare module 'bcryptjs';
+// declare module "dotenv/config";

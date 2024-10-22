1711336de30fdf00eddd23e8e5bad933dd2fdac1
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-22 16:31:51 -0500

    update => update

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -3,12 +3,3 @@ declare module 'express';
 declare module 'cors';
 declare module 'jsonwebtoken';
 declare module 'bcryptjs';
-
-// Extendemos la interfaz Request de Express para incluir las propiedades personalizadas
-declare namespace Express {
-  export interface ExtendsRequest {
-    user?: { id: string };
-    userReferences?: import('../interfaces/props.interface').UserReferencesProps;
-    token?: any;
-  }
-}

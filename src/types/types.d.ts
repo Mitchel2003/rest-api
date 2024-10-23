4ca490ddd8552d2a8a86e3ee68371ac556b93e95
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 15:54:07 -0500

    update => fix one more time

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,6 @@
 import 'mailtrap';
 
-declare namespace Permissions {
+export declare namespace Permissions {
   export type can_read = boolean;
   export type can_update = boolean;
   export type can_destroy = boolean;

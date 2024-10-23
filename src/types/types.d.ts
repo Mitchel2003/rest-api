ef108e379d41afcf9e351f224f82443cf5b48caf
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 16:01:07 -0500

    update => try again

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,17 @@
 import 'mailtrap';
 
-declare namespace Permissions {
-  type can_read = boolean;
-  type can_update = boolean;
-  type can_destroy = boolean;
-  type can_leave = boolean;
+declare namespace MailtrapAPI { 
+  export type Permissions = {
+    can_read: boolean;
+    can_update: boolean;
+    can_destroy: boolean;
+    can_leave: boolean;
+  }
 }
+
+// declare namespace Permissions {
+//   type can_read = boolean;
+//   type can_update = boolean;
+//   type can_destroy = boolean;
+//   type can_leave = boolean;
+// }

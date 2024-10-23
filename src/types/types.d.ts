1c3a1c3773f2c1230961dab4556d93704eaa270c
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 15:51:01 -0500

    update => use global type permissions

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,10 +1,9 @@
 import 'mailtrap';
 
-declare module 'mailtrap' {
-  export type Permissions = {
-    can_read: boolean;
-    can_update: boolean;
-    can_destroy: boolean;
-    can_leave: boolean;
-  }
+
+export type Permissions = {
+  can_read: boolean;
+  can_update: boolean;
+  can_destroy: boolean;
+  can_leave: boolean;
 }

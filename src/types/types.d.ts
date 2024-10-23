1fbac87c265b65fd833e2d3ef3a114a2e34605ad
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 13:06:37 -0500

    Caution => remove types.d.ts

--- src/types/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module 'express';
-declare module 'cors';
-declare module 'cookie-parser';
-declare module 'jsonwebtoken';
-// Añade aquí cualquier otra dependencia que necesite declaración de tipos

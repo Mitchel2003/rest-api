bc9d76da485dd5d89d260ccf3c6708a9f0fb3cef
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 15:49:15 -0500

    update =>  solve mailtrap undefined property

---
+++ src/types/types.d.ts
@@ -0,0 +1,10 @@
+import 'mailtrap';
+
+declare module 'mailtrap' {
+  export type Permissions = {
+    can_read: boolean;
+    can_update: boolean;
+    can_destroy: boolean;
+    can_leave: boolean;
+  }
+}

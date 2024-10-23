0647d6f4471324cf21644eea5afc004fcc74e6b7
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-23 10:55:59 -0500

    update => implement compilation with config babel and mimification with terser

---
+++ src/types/types.d.ts
@@ -0,0 +1,5 @@
+declare module 'express';
+declare module 'cors';
+declare module 'cookie-parser';
+declare module 'jsonwebtoken';
+// Añade aquí cualquier otra dependencia que necesite declaración de tipos

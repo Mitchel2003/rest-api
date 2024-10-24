e9d6ef979e32199156059ee80d1e34085582e123
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-24 14:27:55 -0500

    update => fix Zod error message

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -7,8 +7,12 @@ const validateSchema = (schema: ZodType) => (req: Request, res: Response, next: 
     schema.parse(req.body)
     next()
   } catch (e: unknown) {
-    if (e instanceof ZodError) return send(res, 400, e.message)
+    if (e instanceof ZodError) return send(res, 400, e.message.replace(/[\n\s]/g, ''))
     send(res, 500, `Error interno del servidor al validar el esquema: ${e}`)
+
+    /*
+    "[\n  {\n    \"code\": \"invalid_type\",\n    \"expected\": \"string\",\n    \"received\": \"undefined\",\n    \"path\": [\n      \"email\"\n    ],\n    \"message\": \"El correo electrónico es requerido\"\n  },\n  {\n    \"code\": \"invalid_type\",\n    \"expected\": \"string\",\n    \"received\": \"undefined\",\n    \"path\": [\n      \"password\"\n    ],\n    \"message\": \"La contraseña es requerida\"\n  }\n]"
+     */
   }
 }
 

1e9dc775f30f2121807df26128c3f6d237eae12b
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-24 15:17:14 -0500

    update => try with console.log before find()

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -157,8 +157,11 @@ export const getCountry = async ({ params }: Request, res: Response): Promise<vo
  * Obtiene todos los países.
  * @returns {Promise<void>} - Envía todos los países encontrados o un mensaje de error.
  */
-export const getCountries = async (res: Response): Promise<void> => {
+export const getCountries = async (req: Request, res: Response): Promise<void> => {
   try {
+    const { email, password } = req.body;
+    console.log(email, password);
+    
     const countries = await Country.find();
     send(res, 200, countries);
   } catch (e) { send(res, 500, `Error interno del servidor al obtener los países: ${e}`) }

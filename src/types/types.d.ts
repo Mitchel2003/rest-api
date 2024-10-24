1fc1c7482d66ae06233e2608e5f1052ce17cd1a3
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-24 09:18:23 -0500

    update => using namespace

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,23 +1,39 @@
 // import 'mailtrap';
-
-// Aseguramos que PermissionsApi use nuestra versión de Permissions
-// declare module 'mailtrap/dist/lib/api/resources/Permissions' {
-//   export default class PermissionsApi {
-//     // Métodos que usan nuestra versión de Permissions
-//   }
-// }
 // import { Inbox } from 'mailtrap/dist/types/api/inboxes';
 
-declare module 'mailtrap/dist/types/api/projects.d.ts' {
+export namespace Mailtrap {
 
-  export type Permissions = {
-    can_read: boolean;
-    can_update: boolean;
-    can_destroy: boolean;
-    can_leave: boolean;
+  export namespace Permissions {
+    export type Permissions = {
+      can_read: boolean;
+      can_update: boolean;
+      can_destroy: boolean;
+      can_leave: boolean;
+    }
   }
 
-  export namespace Project {
-    export type permissions = Permissions;
-  }
+  // export namespace Project {
+  //   export type Permissions = {
+  //     can_read: boolean;
+  //     can_update: boolean;
+  //     can_destroy: boolean;
+  //     can_leave: boolean;
+  //   }
+
+  //   export type permissions = Permissions;
+  // }
 }
+
+// declare module 'mailtrap/dist/types/api/projects.d.ts' {
+
+//   export type Permissions = {
+//     can_read: boolean;
+//     can_update: boolean;
+//     can_destroy: boolean;
+//     can_leave: boolean;
+//   }
+
+//   export namespace Project {
+//     export type permissions = Permissions;
+//   }
+// }

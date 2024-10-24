9638c926f9b7d75e5ef3907a0a8eb67e7406fe21
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-24 09:45:13 -0500

    update => using declare module with namespace

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,38 +1,55 @@
 // import 'mailtrap';
-// import { Inbox } from 'mailtrap/dist/types/api/inboxes';
+import { Inbox } from 'mailtrap/dist/types/api/inboxes';
 
-export namespace Mailtrap {
+declare module 'mailtrap/dist/types/api/projects.d.ts' {
+  // Usa el tipo Permissions del namespace Mailtrap.Project
+  export namespace Project {
+    // Definir el tipo ShareLinks dentro del namespace
+    type ShareLinks = {
+      admin: string;
+      viewer: string;
+    };
 
-  export namespace Permissions {
-    export type Permissions = {
+    // Definir el tipo Permissions dentro del namespace
+    type Permissions = {
       can_read: boolean;
       can_update: boolean;
       can_destroy: boolean;
       can_leave: boolean;
-    }
+    };
+
+    // Definir el tipo Project usando los tipos del mismo namespace
+    export type Project = {
+      id: number;
+      name: string;
+      share_links: ShareLinks;
+      inboxes: Inbox[]; // Usar el tipo Inbox desde el namespace
+      permissions: Permissions;  // Usar Permissions del mismo namespace
+    };
+
+    export {};
   }
-
-  // export namespace Project {
-  //   export type Permissions = {
-  //     can_read: boolean;
-  //     can_update: boolean;
-  //     can_destroy: boolean;
-  //     can_leave: boolean;
-  //   }
-
-  //   export type permissions = Permissions;
-  // }
 }
 
-// declare module 'mailtrap/dist/types/api/projects.d.ts' {
 
+// export namespace Mailtrap {
+//   export namespace Permissions {
+//     export type Permissions = {
+//       can_read: boolean;
+//       can_update: boolean;
+//       can_destroy: boolean;
+//       can_leave: boolean;
+//     }
+//   }
+// }
+
+// declare module 'mailtrap/dist/types/api/projects.d.ts' {
 //   export type Permissions = {
 //     can_read: boolean;
 //     can_update: boolean;
 //     can_destroy: boolean;
 //     can_leave: boolean;
 //   }
-
 //   export namespace Project {
 //     export type permissions = Permissions;
 //   }

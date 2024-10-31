7573b61e35841fd06bae9427667aa58cfe0a3544
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-31 09:04:47 -0500

    update => fix init aplication with firebaseConfig

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
 import config from "./config";
 
-const options = JSON.parse(config.firebaseConfig);
-export const app = initializeApp(options);
+export const app = initializeApp(config.firebaseConfig);

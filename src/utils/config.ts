b86d5b1f1c11441149fcda41d7b289e3bb02a48b
mitchel2003 <avilesmaicol.08@gmail.com>
2024-10-31 08:59:30 -0500

    update => enviroment variables (api-auth) - fix .env

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -7,5 +7,14 @@ export default {
   frontendUrl: process.env.FRONTEND_URL as string,
   jwtSecret: process.env.TOKEN_SECRET as string,
   mongodbUri: process.env.MONGODB_URI as string,
-  firebaseConfig: process.env.FIREBASE_CONFIG as string,
+  //firebase config
+  firebaseConfig: {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  }
 };

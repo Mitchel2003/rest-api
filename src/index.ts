edc3f805e22a716f462a082ac9680cb63475e816
Michael Antony <avilesmaicol.08@gmail.com>
2024-11-24 08:56:16 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import "module-alias/register"
 
 import config from "@/utils/config";
-import { mongoDB } from "@/db";
+import { mongoDB } from "@/db"
+import {Request, Response} from "express"
 import app from "@/app";
 
 mongoDB()
 //healthRoute
-app.get('/api/health', (req, res) => { !req.params ?? res.status(200).send('OK') })
+app.get('/api/health', (req: Request, res: Response) => { !req.params.new ?? console.log("anywhere"); res.status(200).send('OK') })
 
 app.listen(config.port, () => console.log(`Server initialized on port ${config.port}`))

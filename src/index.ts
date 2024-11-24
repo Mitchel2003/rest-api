5a8728589111d47fd27ef8adf76cdbd4e375f2ae
Michael Antony <avilesmaicol.08@gmail.com>
2024-11-24 08:42:08 -0500

    update => implements health route (uptime)

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,7 @@ import { mongoDB } from "@/db";
 import app from "@/app";
 
 mongoDB()
+//healthRoute
+app.get('/api/health', (req, res) => { res.status(200).send('OK') })
+
 app.listen(config.port, () => console.log(`Server initialized on port ${config.port}`))

a857cf685595abc40ef9f86a987694c4ec48edca
mitchel2003 <avilesmaicol.08@gmail.com>
2024-12-30 07:13:26 +0100

    UPDATE => fix schema (max characters)

--- src/schemas/user/client.schema.ts
+++ src/schemas/user/client.schema.ts
@@ -4,15 +4,15 @@ export const clientSchema = z.object({
   name: z
     .string({ required_error: "El nombre es requerido" })
     .min(5, { message: "Nombre demasiado corto" })
-    .max(15, { message: "Nombre demasiado largo" }),
+    .max(35, { message: "Nombre demasiado largo" }),
   email: z
     .string({ required_error: "El email es requerido" })
     .email({ message: "Correo electrónico inválido" }),
   phone: z
     .number({ required_error: "El teléfono es requerido" })
     .min(4, { message: "Teléfono demasiado corto" })
-    .max(10, { message: "Teléfono inválido" }),
+    .max(15, { message: "Teléfono inválido" }),
   nit: z
     .string({ required_error: "El nit es requerido" })
-    .min(5, { message: "El nit es inválido" }),
+    .min(15, { message: "El nit es inválido" }),
 })

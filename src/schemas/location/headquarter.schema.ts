f96389bfa0218d77f5d732e97912edff04f78f43
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-03 03:51:50 +0100

    UPDATE => fix schema client and headquarter to be equals to schema frontend (CLIENT and HEADQUARTER)

--- src/schemas/location/headquarter.schema.ts
+++ src/schemas/location/headquarter.schema.ts
@@ -3,14 +3,20 @@ import { z } from "zod";
 const headquarterSchema = z.object({
   name: z
     .string({ required_error: "Nombre de sede requerido" })
-    .min(3, { message: "Nombre de sede debe tener al menos 3 caracteres" }),
+    .min(3, { message: "Nombre de sede debe tener al menos 3 caracteres" })
+    .max(50, { message: "Nombre de sede no puede exceder los 50 caracteres" }),
   address: z
     .string({ required_error: "Dirección de sede requerida" })
-    .min(3, { message: "Dirección de sede debe tener al menos 3 caracteres" }),
+    .min(3, { message: "Dirección de sede debe tener al menos 3 caracteres" })
+    .max(50, { message: "Dirección de sede no puede exceder los 50 caracteres" }),
   city: z
-    .string({ required_error: "Ciudad de sede requerida" }),
+    .string({ required_error: "Ciudad de sede requerida" })
+    .min(3, { message: "Ciudad de sede debe tener al menos 3 caracteres" })
+    .max(50, { message: "Ciudad de sede no puede exceder los 50 caracteres" }),
   client: z
-    .string({ required_error: "Cliente de sede requerido" }),
+    .string({ required_error: "Cliente de sede requerido" })
+    .min(3, { message: "Cliente de sede debe tener al menos 3 caracteres" })
+    .max(50, { message: "Cliente de sede no puede exceder los 50 caracteres" }),
 })
 
 export default headquarterSchema

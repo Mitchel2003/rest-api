9e2e91979e7df6ef27c56ee4a78c5b9f066a0f84
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-03 15:29:02 +0100

    UPDATE => fix issues middleware auth - now throw message (ErrorAPI)

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,16 @@
-import { ExtendsRequest, send } from "@/interfaces/api.interface";
 import { authService as authFB } from "@/services/firebase/auth.service";
+import { ExtendsRequest } from "@/interfaces/api.interface";
+import { handlerResponse } from "@/errors/handler";
 import { NextFunction, Response } from "express";
+import { Unauthorized } from "@/errors/index";
 
 const authRequired = async (req: ExtendsRequest, res: Response, next: NextFunction) => {
-  const authenticate = authFB.onAuth()
-  if (!authenticate) return send(res, 401, 'Usuario no autenticado')
-  req.user = authenticate
-  next()
+  try {
+    const authenticate = authFB.onAuth()
+    if (!authenticate) throw new Unauthorized({ message: 'Usuario no autenticado, por favor inicia sesión' })
+    req.user = authenticate
+    next()
+  } catch (e) { handlerResponse(res, e, "autenticación") }
 }
 
 export default authRequired

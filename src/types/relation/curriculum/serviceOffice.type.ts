f55e01509b697edef63af3f4220ec72a02ff0bea
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-04 03:54:19 +0100

    REMOVE => tables relationship serviceOffice and userHeadquarter (see the MER model database)

--- src/types/relation/curriculum/serviceOffice.type.ts
+++
@@ -1,8 +0,0 @@
-import { Document, Schema } from "mongoose"
-
-export interface ServiceOffice extends Document {//relationship between service and office
-  service: Schema.Types.ObjectId,
-  office: Schema.Types.ObjectId,
-  createdAt?: Date,
-  updatedAt?: Date
-}
--- src/types/relation/engineer/userHeadquarter.type.ts
+++
@@ -1,8 +0,0 @@
-import { Document, Schema } from "mongoose"
-
-export interface UserHeadquarter extends Document {//relationship between user and headquarter
-  user: Schema.Types.ObjectId,
-  headquarter: Schema.Types.ObjectId,
-  createdAt?: Date,
-  updatedAt?: Date
-}

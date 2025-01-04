3eb8df4ef489599316ab0fe379adfdd41d7d7e59
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-04 03:56:01 +0100

    UPDATE => implemented field to office (services)[ ]

--- src/schemas/location/office.schema.ts
+++ src/schemas/location/office.schema.ts
@@ -5,6 +5,9 @@ const officeSchema = z.object({
     .string({ required_error: "Nombre de oficina requerido" })
     .min(3, { message: "Nombre de oficina debe tener al menos 3 caracteres" })
     .max(50, { message: "Nombre de oficina no puede exceder los 50 caracteres" }),
+  services: z
+    .array(z.string({ required_error: "Servicio requerido" }))
+    .min(1, { message: "Debe seleccionar al menos un servicio" }),
   area: z
     .string({ required_error: "Área requerida" })
 })
--- src/types/location/office.type.ts
+++ src/types/location/office.type.ts
@@ -2,6 +2,7 @@ import { Document, Schema } from "mongoose"
 
 export interface Office extends Document {
   name: string,
+  services: string[],
   area: Schema.Types.ObjectId,
   createdAt?: Date,
   updatedAt?: Date

09cb635d4caf16d3b3fcd338f99274d7b2f6e974
mitchel2003 <avilesmaicol.08@gmail.com>
2025-01-17 20:29:15 +0100

    UPDATE => implementing debuger on console render

--- src/services/mongodb/form/curriculum/accessory.service.ts
+++ src/services/mongodb/form/curriculum/accessory.service.ts
@@ -2,9 +2,13 @@ import { Doc, Query, Populate } from "@/types/repository.type";
 import Repository from "@/repositories/mongodb.repository";
 import MongoDB from "@/services/mongodb/mongodb.service";
 import { Result } from "@/interfaces/api.interface";
+import debug from 'debug';
 
 import accessoryModel from "@/models/form/curriculum/accessory.model";
 import { Accessory } from "@/types/form/curriculum/accessory.type";
+
+// Configurar el logger
+const log = debug('app:accessory');
 
 class AccessoryService extends MongoDB<Accessory> {
   private static instance: AccessoryService;
@@ -51,14 +55,22 @@ class AccessoryService extends MongoDB<Accessory> {
 
   // Overwrite the methods to apply the populate that corresponds to this service "accessory"
   async find(query?: Query, populate?: Populate): Promise<Result<Accessory[]>> {
-    console.log(query)
-    return super.find(query, populate || this.defaultPopulate);
+    log('AccessoryService.find called with:', { query, populate });
+    const result = await super.find(query, populate || this.defaultPopulate);
+    log('AccessoryService.find result:', result);
+    return result;
   }
   async findById(id: string): Promise<Result<Accessory | null>> {
-    return super.findById(id, this.defaultPopulate);
+    log('AccessoryService.findById called with:', { id });
+    const result = await super.findById(id, this.defaultPopulate);
+    log('AccessoryService.findById result:', result);
+    return result;
   }
   async update(id: string, data: Partial<Doc<Accessory>>): Promise<Result<Accessory | null>> {
-    return super.update(id, data, this.defaultPopulate);
+    log('AccessoryService.update called with:', { id, data });
+    const result = await super.update(id, data, this.defaultPopulate);
+    log('AccessoryService.update result:', result);
+    return result;
   }
 }
 

93ed6ba105e8a29973a27976aa93a619ecca016f
mitchel2003 <avilesmaicol.08@gmail.com>
2025-04-01 05:21:43 +0200

    [REMOVE_PUPPETEER => to message whatsapp (dont works)]

--- src/routes/auth/auth.routes.ts
+++ src/routes/auth/auth.routes.ts
@@ -1,7 +1,6 @@
-import { login, logout, forgotPassword, getOnAuth, sendMessage } from "@/controllers/auth/auth.controller"
+import { login, logout, forgotPassword, getOnAuth } from "@/controllers/auth/auth.controller"
 import { loginSchema, forgotPasswordSchema } from "@/schemas/auth/auth.schema"
 import validateSchema from "@/middlewares/validator.middleware"
-import authRequired from "@/middlewares/auth.middleware"
 import { Router } from "express"
 
 const router = Router()
@@ -10,6 +9,5 @@ router.post('/logout', logout)// authentication
 router.post('/login', validateSchema(loginSchema), login)
 router.get('/on-auth', getOnAuth)// verification
 router.post('/forgot-password', validateSchema(forgotPasswordSchema), forgotPassword)
-router.post('/send-message', authRequired, sendMessage)// message whatsapp
 
 export default router
